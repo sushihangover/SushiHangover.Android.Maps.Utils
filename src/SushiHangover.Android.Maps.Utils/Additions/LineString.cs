81a7a1e4c913f9ef5c21e9fb7f4c2e4a76c7a199
SushiHangover/R.Nees <sushihangover@outlook.com>
2017-08-17 23:07:22 -0700

    Revert "Convert:"
    This reverts commit 7bdd2dbfcc46893955e1b4dfebfb14f005b1978f.

--- src/SushiHangover.Android.Maps.Utils/Additions/LineString.cs
+++ src/SushiHangover.Android.Maps.Utils/Additions/LineString.cs
@@ -4,8 +4,6 @@ namespace Android.Gms.Data
 {
 	public partial class LineString
 	{
-		//public java.util.ArrayList<LatLng> getGeometryObject()
-		public unsafe IList<Maps.Model.LatLng> GeometryObject => new Runtime.JavaArray<Maps.Model.LatLng>(GeometryJavaObject().Handle, Android.Runtime.JniHandleOwnership.TransferLocalRef);
-
+		public unsafe IList<Maps.Model.LatLng> GeometryObject => GeometryJavaObject() as IList<Maps.Model.LatLng>;
 	}
 }

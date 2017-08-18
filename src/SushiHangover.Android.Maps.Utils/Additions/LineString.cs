7bdd2dbfcc46893955e1b4dfebfb14f005b1978f
SushiHangover/R.Nees <sushihangover@outlook.com>
2017-08-17 22:54:07 -0700

    Convert:   java.util.ArrayList<LatLng> getGeometryObject() to:   IList<Maps.Model.LatLng>

--- src/SushiHangover.Android.Maps.Utils/Additions/LineString.cs
+++ src/SushiHangover.Android.Maps.Utils/Additions/LineString.cs
@@ -4,6 +4,8 @@ namespace Android.Gms.Data
 {
 	public partial class LineString
 	{
-		public unsafe IList<Maps.Model.LatLng> GeometryObject => GeometryJavaObject() as IList<Maps.Model.LatLng>;
+		//public java.util.ArrayList<LatLng> getGeometryObject()
+		public unsafe IList<Maps.Model.LatLng> GeometryObject => new Runtime.JavaArray<Maps.Model.LatLng>(GeometryJavaObject().Handle, Android.Runtime.JniHandleOwnership.TransferLocalRef);
+
 	}
 }
